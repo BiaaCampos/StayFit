688d0584fe8df4d20a6fa3d9b40d0a14ad1536f5
GiihGabi <giihgabi@icloud.com>
2024-09-09 17:16:41 -0300

    Backend do cadastro nutricional, iniciada. Falta fazer alguns tratamentos nos inputs, ajustar o css. Adicionei novas tabelas no banco de dados não postei nessa branch por conta de testes por enquanto

--- public/components/cad_nutricional/historicomedico.js
+++ public/components/cad_nutricional/historicomedico.js
@@ -1,30 +1,67 @@
 Vue.component("historicomedico", {
     props: ['tipohistoricomedico'],
     template: `
-<div>
-    <form @submit.prevent="submitForm">
-        <div class="form-group">
-            <div class="number">
-                1
+    <div>
+        <form @submit.prevent="submitForm">
+            <div v-for="(pergunta, index) in perguntas" :key="pergunta.id" class="form-group">
+                <label :for="'pergunta-' + pergunta.id">{{ pergunta.texto_pergunta }}</label>
+                <ejs-textbox 
+                    v-model="respostas[pergunta.id]" 
+                    :id="'pergunta-' + pergunta.id" 
+                    floatLabelType="Auto" 
+                    placeholder="Resposta"
+                ></ejs-textbox>
             </div>
-            <div>
-                <label for="q1">
-                    Qual é o seu nome?
-                </label>
-                <ejs-textbox floatLabelType="Auto" placeholder=""></ejs-textbox>
-            </div>
-            <div>
-                <span class="help-icon">
-                    <img src="public/images/images-cadastros/question.svg" alt="logo" />
-                    <span class="tooltip">Digite seu nome completo.</span>
-                </span>
-            </div>
-        </div>
-    </form>
-</div>`,
+            <button type="submit">Submit</button>
+        </form>
+    </div>`,
+    data() {
+        return {
+            perguntas: [],
+            respostas: {},
+            tipo_id: 1,
+        };
+    },
     methods: {
+        listaPerguntasHistorico() {            
+            axios.post(BASE + "/cadastronutricional/listaPerguntasHistorico", { tipo_id: this.tipo_id })
+            .then((res) => {
+                this.perguntas = res.data;
+                this.perguntas.forEach(pergunta => {
+                    this.$set(this.respostas, pergunta.id, '');
+                });
+            })
+            .catch((error) => {
+                console.error('Erro ao buscar perguntas:', error);
+            });
+        },
         submitForm() {
-            this.$emit('next');
+            const data = {
+                id_paciente: 1,
+                respostas: Object.keys(this.respostas).map(id => ({
+                    id_pergunta: id,
+                    resposta: this.respostas[id]
+                })),
+                id_usuario_modificacao: 1
+            };
+        
+            console.log('Dados enviados para o backend:', data);
+        
+            axios.post(BASE + '/cadastronutricional/salvarRespostas', data)
+            .then((res) => {
+                if (res.data.code === 1) {
+                    this.$emit('submit-form');
+                } else {
+                    alert('Erro ao salvar respostas.');
+                }
+            })
+            .catch((error) => {
+                console.error('Erro ao salvar respostas:', error);
+            });
         }
+        
+    },
+    mounted() {
+        this.listaPerguntasHistorico(); 
     }
 });
