a7554d08351333f9f38fda73b60630491ec7c3ce
Vinícius Campos <viniciuscampos1408@gmail.com>
2024-10-01 15:59:46 -0300

    quase terminado

--- views/agendarconsulta/app.vue.js
+++ views/agendarconsulta/app.vue.js
@@ -5,6 +5,7 @@ const newLocal = `
             <h1>Agendar Consulta</h1>
             <h6>Confira os profissionais</h6>
         </div>
+
         <!-- Card Section -->
         <div class="col-lg-12 control-section card-control-section basic_card_layout">
             <div class="e-card-resize-container">       
@@ -28,7 +29,8 @@ const newLocal = `
                                         <!-- Card content can be added here -->
                                     </div>
                                     <div class="e-card-actions">
-                                        <button class="e-btn e-outline e-primary" role="button">
+                                        <modal_info ref="modal" :tipomodalinfo="tipomodalinfo"></modal_info>
+                                        <button class="e-btn e-outline e-primary" role="button" @click="$refs.modal.openModal()">
                                             Selecionar
                                         </button>
                                     </div>
@@ -39,11 +41,51 @@ const newLocal = `
                 </div>        
             </div>
         </div>
+
+        <!-- Repetir a seção de Card conforme necessário -->
+        <div class="col-lg-12 control-section card-control-section basic_card_layout">
+            <div class="e-card-resize-container">       
+                <div class='row'>
+                    <div class="row card-layout">
+                        <div class="col-xs-6 col-sm-6 col-lg-6 col-md-6">
+                            <!-- Basic Card Layout -->
+                            <div class="card-profissionais-container">
+                                <div tabindex="0" class="e-card card-profissionais" id="basic_card" role="button">
+                                    <div class="e-card-header">
+                                        <div class="e-card-header-caption">
+                                            <div class="title-agendar">Dr(a) Teste Unimar</div>
+                                            <div class="e-card-sub-title"><strong>CRN:</strong> 00000 - <strong>RQE</strong> Nº </div>
+                                            <div class="line-card">
+                                                <div class="line"></div> <!-- Linha fininha -->
+                                            </div>
+                                            <div class="e-card-sub-title">Consulte a clínica sobre os critérios de atendimento</div>
+                                        </div>
+                                    </div>
+                                    <div class="e-card-content">
+                                        <!-- Card content can be added here -->
+                                    </div>
+                                    <div class="e-card-actions">
+                                        <modal_info ref="modal2" :tipomodalinfo="tipomodalinfo"></modal_info>
+                                        <button class="e-btn e-outline e-primary" role="button" @click="$refs.modal2.openModal()">
+                                           <modal_atendimento :tipomodalatendimento="tipomodalatendimento"></modal_atendimento> 
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+            </div>
+        </div>
+
+        <!-- Linear Progress Bar -->
+        <div id="linearsegment" style="margin: 20px auto; width: 50%;">
+            <!-- Linear Progress Bar will be appended here -->
+        </div>
         <!-- End Card Section -->
     </div>    
 </div>
 `;
-
 
 const AppTemplate = newLocal;
 
@@ -54,10 +96,25 @@ Vue.component('AppVue', {
             data: [],
             pageSettings: { pageCount: 5 },
             toolbar: ["Search"],
+            currentPage: 0,
+            totalPages: 3,
+            errorProgress: null,
+            tipomodalinfo: "Aqui estão as informações do modal!", // Mensagem para o modal
+            tipomodalatendimento: null
         }
     },
-    methods: {
-    },
     mounted: function() {
+        this.errorProgress = new ej.progressbar.ProgressBar({
+            type: 'Linear',
+            height: '30',
+            segmentCount: 4,
+            value: 25,
+            animation: {
+                enable: true,
+                duration: 2000,
+                delay: 0,
+            }
+        });
+        this.errorProgress.appendTo('#linearsegment');
     }
 });
