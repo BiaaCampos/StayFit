8c1a8c4a37a63c3fffaecd94f25ccd35bde90fce
Giovanna Batista <101181715+GiihGabi@users.noreply.github.com>
2024-12-03 14:18:36 -0300

    Update Database.php

--- libs/Database.php
+++ libs/Database.php
@@ -26,19 +26,10 @@ class Database extends PDO
 
     }
 
-    /**
-     * select
-     * @param string $sql : comando sql
-     * @param array $array parametros e dados para o bind
-     * @param constant $fetchMode modo de fetch: PDO::FETCH_OBJ, PDO::FETCH_ASSOC, PDO::FETCH_BOTH...
-     * @return todos os registros encontrados
-     */
     public function select($sql, $array = array(), $fetchMode = PDO::FETCH_OBJ)
     {
 
         $sth = $this->prepare($sql);
-        //percorre os elementos do array para fazer o bind de cada parametro
-        //com o respectivo valor
         foreach ($array as $key => $value) {
             $sth->bindValue("$key",$value);
         }
@@ -47,17 +38,9 @@ class Database extends PDO
         return $sth->fetchAll($fetchMode);
     }
 
-    /**
-     * insert
-     * @param string $table : tabela em que o dado sera inserido
-     * @param string $data  : um array contendo os campos e valores
-     */
     public function insert($table, $data)
     {
-        //ordena o array pelas chaves (nome do campo)
         ksort($data);
-
-        //monta a string do prepare com os campos e valores
 
         $fieldNames = implode('`, `', array_keys($data));
         $fieldValues = ':' . implode(', :', array_keys($data));
@@ -65,42 +48,28 @@ class Database extends PDO
 
         $sth = $this->prepare("INSERT INTO $table (`$fieldNames`) VALUES ($fieldValues)");
 
-        //percorre o array criando os parametros (:campo) e fazendo o bind com os respectivos valores
         foreach ($data as $key => $value) {
             $sth->bindValue(":$key", $value);
         }
-        //executa o insert
         $sth->execute();
-        //retorna o id do ultimo registro
         return Database::lastInsertId();
     }
 
-    /**
-     * update
-     * @param string $table : tabela que deseja alterar os dados
-     * @param string $data  : um array contendo os campos e valores
-     * @param string $where : condicao do update
-     */
     public function update($table, $data, $where)
     {
-        //ordena o array pelas chaves (nome do campo)
         ksort($data);
 
         $fieldDetails = NULL;
-        //monta a string do prepare com os campos do set com os parametros (:campo)
         foreach($data as $key=> $value) {
             $fieldDetails .= "`$key`=:$key,";
         }
-        //retira a ultima virgula
         $fieldDetails = rtrim($fieldDetails, ',');
 
         $sth = $this->prepare("UPDATE $table SET $fieldDetails WHERE $where");
 
-        //percorre o array criando os parametros (:campo) e fazendo o bind com os respectivos valores
         foreach ($data as $key => $value) {
             $sth->bindValue(":$key", $value);
         }
-        //executa o update
         $sth->execute();
 
         if ($sth->execute()) {
@@ -108,17 +77,8 @@ class Database extends PDO
         }
     }
 
-    /**
-     * delete
-     *
-     * @param string $table
-     * @param string $where
-     * @param integer $limit
-     * @return integer linhas afetadas
-     */
     public function delete($table, $where, $limit = 1)
     {
-        //ainda falta implementar mais recursos e pode-se fazer as mesmas verificacoes do update com prepare
         return $this->exec("DELETE FROM $table WHERE $where");
     }
 
