298e26892ef96862f5272b0c2a03d7b0a6c00d5a
Giovanna Batista <101181715+GiihGabi@users.noreply.github.com>
2024-12-03 14:19:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,14 @@
 
 require 'config.php';
 
-// funcao que carrega as classes automaticamente
 spl_autoload_register('myAutoloader');
 
 function myAutoloader($class) {
-    /*caso ignorar load da classe*/
     if (strpos($class, 'Fpdi') === false) {
         require_once LIBS . $class .".php";
     }
 }
 
-// carrega o bootstrap - inicializador
 $bootstrap = new Bootstrap();
 
-// caminhos opcionais
-//$bootstrap->setControllerPath();
-//$bootstrap->setModelPath();
-//$bootstrap->setDefaultFile();
-//$bootstrap->setErrorFile();
 $bootstrap->init();
