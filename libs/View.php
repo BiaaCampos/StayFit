52dd2feb87857838c4e7092a8246f38be1632005
Giovanna Batista <101181715+GiihGabi@users.noreply.github.com>
2024-12-03 14:18:50 -0300

    Update View.php

--- libs/View.php
+++ libs/View.php
@@ -1,14 +1,12 @@
 <?php
 
 class View {
-    // Titulo da View (pagina)
     public $title = "Titulo";
 
     function __construct()
     {
         
     }
-    // renderiza a pagina com o nome da view passado no parametro
     public function render($name, $noInclude = false)
     {
         require 'views/' . $name . '.php';
